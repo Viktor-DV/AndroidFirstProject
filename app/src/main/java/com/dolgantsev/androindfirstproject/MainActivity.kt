75d528e391dbdc1248e3f085aa3b9624065d1628
Viktor-DV <Viktor45v2@gmail.com>
2024-12-02 15:53:38 +0500

    Third0 set of tasks, three animation of central posters, each executed in different way was added.

--- app/src/main/java/com/dolgantsev/androindfirstproject/MainActivity.kt
+++ app/src/main/java/com/dolgantsev/androindfirstproject/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.dolgantsev.androindfirstproject
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.animation.LinearInterpolator
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +26,9 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+
         val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottom_navigation.setOnItemSelectedListener {
-
             when (it.itemId) {
                 R.id.favorites -> {
                     Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
@@ -43,5 +46,36 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        animateCard(findViewById(R.id.poster_1))
+        animateCard(findViewById(R.id.poster_2))
+        animateCard(findViewById(R.id.poster_3))
+        animateCard(findViewById(R.id.poster_4))
+    }
+
+    private fun animateCard(card: CardView) {
+        val moveUp = ObjectAnimator.ofFloat(card, "translationY", 0f, -10f).apply {
+            duration = 1000
+            interpolator = LinearInterpolator()
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }
+
+        val rotate = ObjectAnimator.ofFloat(card, "rotation", 0f, 2f).apply {
+            duration = 1500
+            interpolator = LinearInterpolator()
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }
+
+        val scale = ObjectAnimator.ofFloat(card, "scaleX", 1f, 1.05f).apply {
+            duration = 1200
+            interpolator = LinearInterpolator()
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(moveUp, rotate, scale)
+        animatorSet.start()
     }
 }
