bd7a1607f3fba0e81b7179cc28a1d378b587cc57
Viktor-DV <Viktor45v2@gmail.com>
2025-02-27 18:40:02 +0500

    добавлен рейтинг вильмов, добавлена анимация рейтинга при его появлении

--- app/src/main/java/com/dolgantsev/androindfirstproject/FavoritesFragment.kt
+++ app/src/main/java/com/dolgantsev/androindfirstproject/FavoritesFragment.kt
@@ -1,6 +1,5 @@
 package com.dolgantsev.androindfirstproject
 
-import FilmListRecyclerAdapter
 import TopSpacingItemDecoration
 import android.os.Bundle
 import android.view.LayoutInflater
--- app/src/main/java/com/dolgantsev/androindfirstproject/FilmViewHolder.kt
+++ app/src/main/java/com/dolgantsev/androindfirstproject/FilmViewHolder.kt
@@ -1,31 +1,40 @@
+package com.dolgantsev.androindfirstproject
+
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.dolgantsev.androindfirstproject.Film
-import com.dolgantsev.androindfirstproject.R
 
-//В конструктор класс передается layout, который мы создали(film_item.xml)
 class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    //Привязываем View из layout к переменным
+    // Привязываем View из layout к переменным
     private val title: TextView = itemView.findViewById(R.id.title)
     private val poster: ImageView = itemView.findViewById(R.id.poster)
     private val description: TextView = itemView.findViewById(R.id.description)
+    private val ratingDonut: RatingDonutView = itemView.findViewById(R.id.rating_donut) // Исправлен тип на RatingDonutView
 
-    //В этом методе кладем данные из Film в наши View
+    // Флаг для отслеживания, была ли анимация уже запущена
+    private var isAnimated = false
+
+    // В этом методе кладем данные из Film в наши View
     fun bind(film: Film) {
-        //Устанавливаем заголовок
+        // Устанавливаем заголовок
         title.text = film.title
-        //Устанавливаем описание
+        // Устанавливаем описание
         description.text = film.description
-        //Указываем контейнер, в котором будет "жить" наша картинка
+        // Указываем контейнер, в котором будет "жить" наша картинка
         Glide.with(itemView)
-            //Загружаем сам ресурс
             .load(film.poster)
-            //Центруем изображение
             .centerCrop()
-            //Указываем ImageView, куда будем загружать изображение
             .into(poster)
+
+        // Настраиваем рейтинг с анимацией
+        if (!isAnimated) {
+            ratingDonut.setProgress(0) // Сбрасываем прогресс перед анимацией
+            ratingDonut.setProgress((film.rating * 10).toInt(), animate = true) // Запускаем анимацию один раз
+            isAnimated = true // Устанавливаем флаг, чтобы анимация не повторялась
+        } else {
+            ratingDonut.setProgress((film.rating * 10).toInt(), animate = false) // Без анимации при повторном bind
+        }
     }
 }
